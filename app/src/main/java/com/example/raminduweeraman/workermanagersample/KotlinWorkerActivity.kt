d7eeeff9679d3c47f6407a62ffbdcae8a78f44e5
Ramindu Weeraman <ramindu.weeraman@gmail.com>
2019-03-25 22:23:49 +0800

    refactored.

--- app/src/main/java/com/example/raminduweeraman/workermanagersample/KotlinWorkerActivity.kt
+++ app/src/main/java/com/example/raminduweeraman/workermanagersample/KotlinWorkerActivity.kt
@@ -12,7 +12,6 @@ import java.util.concurrent.TimeUnit
 class KotlinWorkerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("TAG","-------onCreate2")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_worker)
         btn1.setOnClickListener{
@@ -21,42 +20,13 @@ class KotlinWorkerActivity : AppCompatActivity() {
 
     }
 
-    override fun onStart() {
-        Log.d("TAG","-------onStart2")
-        super.onStart()
-    }
-
-    override fun onRestart() {
-        Log.d("TAG","-------onRestart2")
-        super.onRestart()
-    }
-
-    override fun onResume() {
-        Log.d("TAG","-------onResume2")
-        super.onResume()
-    }
-
-    override fun onPause() {
-        Log.d("TAG","-------onPause2")
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d("TAG","-------onStop2")
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.d("TAG","-------onDestroy2")
-        super.onDestroy()
-    }
 
     private fun startBackgroundWork() {
         Log.d("TAG", "************ startBackgroundWork")
         val photoCheckBuilder =
                 PeriodicWorkRequestBuilder<Worker>(15, TimeUnit.MINUTES)
         val photoCheckWork = photoCheckBuilder.build()
-        WorkManager.getInstance().enqueueUniquePeriodicWork("OBC_DATA_SYNC",
+        WorkManager.getInstance().enqueueUniquePeriodicWork("BACKGROUND_WORKER",
                 ExistingPeriodicWorkPolicy.REPLACE, photoCheckWork)
     }
 
